e7abd7e98e9dbef806c2807ef688650df6399425
dilanrinconhbtn <937@holbertonschool.com>
2019-11-13 17:05:33 +0000

    console.py

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Console
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """class HBNBCommand"""
+
+    prompt = '(hbnb)'
+
+    def do_EOF(self, line):
+        """exit"""
+        return True
+
+    def do_quit(self, line):
+        """exit"""
+        return True
+
+    def emptyline(self):
+        '''Empty'''
+        pass
