6adb6dbdb3cbf2c16a8f28d12df57cd4d9ba84a3
Jimmer942 <942@holbertonschool.com>
2019-11-13 12:45:06 -0500

    Classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,10 +4,17 @@
 
 
 import json
-import os
+import os.path
 from models.base_model import BaseModel
-
-Jsondic = {'BaseModel': BaseModel}
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+Jsondic = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
+           'State': State, 'City': City, 'Amenity': Amenity,
+           'Review': Review}
 
 
 class FileStorage:
