be1515330bf4716bcbf2436e04d70d95ee9ec12c
Jimmer942 <942@holbertonschool.com>
2019-11-12 22:30:42 -0500

    Base

--- models/base_model.py
+++ models/base_model.py
@@ -5,13 +5,17 @@ from datetime import *
 
 
 class BaseModel():
+    """ Class Basemodel """
+
     def __init__(self, *args, **kwargs):
+        """ Init """
         if len(kwargs) == 0:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.today()
             self.updated_at = datetime.today()
 
     def __str__(self):
+        """ Print """
         return("[{}] ({}) {}".format(self.__class__.__name__, self.id,
                                      self.__dict__))
 
@@ -20,6 +24,7 @@ class BaseModel():
         self.updated_at = datetime.utcnow()
 
     def to_dict(self):
+        """ Dictionary """
         my_dict = self.__dict__
         my_dict['created_at'] = self.created_at.isoformat()
         my_dict['updated_at'] = self.updated_at.isoformat()
